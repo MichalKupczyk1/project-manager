8c6cd0df5010344755f5eae166e69071e31df17a
MichalKupczyk1 <72615252+MichalKupczyk1@users.noreply.github.com>
2025-03-15 13:14:57 +0100

    Added migration files after updating one-to-many relation between users and projects

---
+++ ProjectManager.Database/Migrations/20250314210749_OptionalRelationship.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManager.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class OptionalRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_project_user_owner_id",
+                table: "project");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "owner_id",
+                table: "project",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_project_user_owner_id",
+                table: "project",
+                column: "owner_id",
+                principalTable: "user",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_project_user_owner_id",
+                table: "project");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "owner_id",
+                table: "project",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_project_user_owner_id",
+                table: "project",
+                column: "owner_id",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
